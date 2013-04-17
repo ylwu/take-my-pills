e18e9e97ae79b6d062c6eff17172aead12bfc124
Morgan Lai <morgan.yian.lai@gmail.com>
2013-04-17 17:59:15 -0400

    comment

---
+++ src/log_in.js
@@ -0,0 +1,8 @@
+   $(document).ready(function(){
+		$(app_log_in_sign_up_btn).click(function()    {
+            
+                window.location="patien_sign_up.html";
+
+        });
+
+   });
---
+++ src/sign_up.js
@@ -0,0 +1,10 @@
+   $(document).ready(function(){
+
+  		$(app_sign_up_log_in_btn).click(function()    {
+            
+                window.location="patient_log_in.html";
+
+        });      
+
+
+   });
