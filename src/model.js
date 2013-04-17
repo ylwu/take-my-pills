e2f2aef9ac26bb62f27f2407f2ed321626390caf
Yonglin Wu <yonglinwu92@gmail.com>
2013-04-17 02:48:51 -0400

    separate js file

--- src/model.js
+++ src/model.js
@@ -3,6 +3,38 @@ var Model = function(){
 	this.drugsQueue = [];
 	this.displayQueue = [];
 	this.actionQueue = [];
+
+	this.allHandlers = new Array();
+	
+	/**
+	 * Dispatch a new event to all the event listeners of a given event type
+	 */
+	this.dispatchEvent = function(type, details){
+		var newEvent = new BoardEvent(type, details);
+
+		if (this.allHandlers[type]){
+			for (var i in this.allHandlers[type]){
+				this.allHandlers[type][i](newEvent);
+			}
+		}
+	}
+
+	/**
+	 * Add a new event listener for a given event type
+	 * the parameter 'handler' has to be a function with one parameter which is an event object
+	 */
+	this.addEventListener = function(eventType, handler){
+		if (!this.allHandlers[eventType])
+			this.allHandlers[eventType] = [];
+		this.allHandlers[eventType].push(handler);
+	}
+
+	this.nextTenDrugs = function(){
+
+	}
+
+
+
 }
 
 
