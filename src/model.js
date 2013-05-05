bc289254891c71e2e9c256407d93c236e201bb19
Ty <ty@ubuntu.(none)>
2013-05-05 13:49:30 -0400

    Revert to Jason's - Ty

--- src/model.js
+++ src/model.js
@@ -43,19 +43,12 @@ var Model = function(){
 	}
 
 	this.dateToTimeString = function(date){
-		min = date.getMinutes().toString();
-		hour = date.getHours();
-		if (min.length == 1){
-			min+= "0";
+		d = date.getMinutes().toString();
+		console.log(d.length);
+		if (d.length == 1){
+			d+= "0";
 		}
-
-		if (hour<12){
-			return hour.toString() + " : " + min + " am"
-		} else if (hour == 12){
-			return 12 + " : " + min + " pm"
-		} else {
-			return (hour-12).toString() + " : " + min + " pm"
-		}
+		return date.getHours() + " : " + d;
 	}
 
 	this.initDrug = function(x){
@@ -83,21 +76,11 @@ var Model = function(){
 							});
 						}
 				}
-			} else if (x.frequency == 0){                       //cycle drugs
-
+			} else if (x.frequency == 2){                       //cycle drugs
 				if ((this.startDate(x) <= this.curtime) && 
 					(this.endDate(x) >= this.curtime)){
-				if (x.nextPillTime[0] == -1){
-				drugEventTime = new Date(this.curtime.getFullYear(), this.curtime.getMonth(),this.curtime.getDate(),0,0);
-				this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),"Start Cycle","past"));
-				} else {
-					drugEventTime = new Date(this.curtime.getFullYear(), this.curtime.getMonth(),this.curtime.getDate(),x.nextPillTime[0],x.nextPillTime[1]);
-					if (drugEventTime < this.curtime){
-								this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),this.dateToTimeString(drugEventTime),"past"));
-							} else {
-								this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),this.dateToTimeString(drugEventTime),"future"));
-							}
-				}
+				fdate = new Date(this.curtime.getFullYear(), this.curtime.getMonth(),this.curtime.getDate(),9,0);
+				this.displayQueue.push(new DrugEvent(x.name,fdate,x.dose,this.dateToDateString(fdate),x.times.toString()+" times","future"));
 				this.displayQueue.sort(function(a,b)
 							{
 								return (a.date - b.date);
@@ -106,8 +89,8 @@ var Model = function(){
 			} else {                                            //times per day drug
 				if ((this.startDate(x) <= this.curtime) && 
 					(this.endDate(x) >= this.curtime)){
-				drugEventTime = new Date(this.curtime.getFullYear(), this.curtime.getMonth(),this.curtime.getDate(),9,0);
-				this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),x.times.toString()+" times","future"));
+				fdate = new Date(this.curtime.getFullYear(), this.curtime.getMonth(),this.curtime.getDate(),x.lasttake[0],x.lasttake[1]);
+				this.displayQueue.push(new DrugEvent(x.name,fdate,x.dose,this.dateToDateString(fdate),x.lasttake[0].toString()+" : "+x.lasttake[1].toString()+"0 am","future"));
 				this.displayQueue.sort(function(a,b)
 							{
 								return (a.date - b.date);
@@ -118,7 +101,8 @@ var Model = function(){
 
 
 	this.moreDrug = function(x){
-		if (x.frequency == 1){                                //specific time drugs
+		this.newtime = new Date(2013,3, this.newtime.getDate()+1,0,0,0,0);
+			if (x.frequency == 1){
 				if ((this.startDate(x) <= this.newtime) && 
 					(this.endDate(x) >= this.newtime)){
 						times = this.stringtoTime(x.times);
@@ -130,43 +114,35 @@ var Model = function(){
 							if (ex == "pm" && (! ((hour == 12) && (min == 0)))){
 								hour += 12;
 							}
-							drugEventTime= new Date(this.newtime.getFullYear(), this.newtime.getMonth(),this.newtime.getDate(),hour,min);
-							if (drugEventTime < this.newtime){
-								this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),times[j],"past"));
-							} else {
-								this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),times[j],"future"));
-							}
+							fdate= new Date(this.newtime.getFullYear(), this.newtime.getMonth(),this.newtime.getDate(),hour,min);
+							this.displayQueue.push(new DrugEvent(x.name,fdate,x.dose,this.dateToDateString(fdate),times[j],"future"));
 							this.displayQueue.sort(function(a,b)
 							{
 								return (a.date - b.date);
 							});
 						}
 				}
-			} else if (x.frequency == 0){                       //cycle drugs
-			} else {                                            //times per day drug
+			} else if (x.frequency == 2){
 				if ((this.startDate(x) <= this.newtime) && 
 					(this.endDate(x) >= this.newtime)){
-				drugEventTime = new Date(this.newtime.getFullYear(), this.newtime.getMonth(),this.newtime.getDate(),9,0);
-				this.displayQueue.push(new DrugEvent(x.name,drugEventTime,x.dose,this.dateToDateString(drugEventTime),x.times.toString()+" times","future"));
+				fdate = new Date(this.newtime.getFullYear(), this.newtime.getMonth(),this.newtime.getDate(),9,0);
+				this.displayQueue.push(new DrugEvent(x.name,fdate,x.dose,this.dateToDateString(fdate),x.times.toString()+" times","future"));
 				this.displayQueue.sort(function(a,b)
 							{
 								return (a.date - b.date);
 							});
+			    }
 			}
-		}
-			
 	}
 
 	this.initDrugs = function(){
 		this.displayQueue.length = 0;
 		for (var i=0; i<this.drugsQueue.length; i++){
 			this.initDrug(this.drugsQueue[i]);
-			console.log(this.drugsQueue[i]);
 	}
 	}
 
 	this.moreDrugs= function(){
-		this.newtime = new Date(2013,this.newtime.getMonth(),this.newtime.getDate()+1,0,0,0,0);
 		for (var i=0; i<this.drugsQueue.length; i++){
 			this.moreDrug(this.drugsQueue[i]);
 	}
--- src/utils.js
+++ src/utils.js
@@ -6,32 +6,32 @@ var myJsonPills;
 
 function returnAllDrugs(){
       var t;
-            $.ajax({
-                  dataType: "json",
-                  url: 'pilldata.json',
-                  async: false,
-                  success: function(data){
+			$.ajax({
+  				dataType: "json",
+  				url: 'pilldata.json',
+  				async: false,
+  				success: function(data){
             t = data;
-                }
-            });
+				}
+			});
       return t;
 
-        }
+		}
 
 
 function loadDrugs(){
-            $.ajax({
-                  dataType: "json",
-                  url: 'pilldata.json',
-                  async: false,
-                  success: function(data){
-                      m.drugsQueue.length = 0;
-                      data.forEach(function(entry){
-                          m.drugsQueue.push(entry);
-                      })
-                }
-            });
-        }
+			$.ajax({
+  				dataType: "json",
+  				url: 'pilldata.json',
+  				async: false,
+  				success: function(data){
+  					m.drugsQueue.length = 0;
+  					data.forEach(function(entry){
+  						m.drugsQueue.push(entry);
+  					})
+				}
+			});
+		}
 
 function returnAllMessages(){
   var s;
@@ -75,169 +75,171 @@ function returnAllPatients(){
 
 // TY_GLOBAL WRITE_TO_JSON FUNCTIONS
 
-    function pillToJsonString(aPill) {
-        var pillString='{"name": "'+aPill.name+'", "dose": "'+aPill.dose+'", "startdate": "'+aPill.startdate+'", "enddate": "'+aPill.enddate+'", "frequency": "'+aPill.frequency+'", "times": "'+aPill.times+'", "lasttake": ['+aPill.lasttake+']}';
-        return pillString;
-    }
+	function pillToJsonString(aPill) {
+		var pillString='{"name": "'+aPill.name+'", "dose": "'+aPill.dose+'", "startdate": "'+aPill.startdate+'", "enddate": "'+aPill.enddate+'", "frequency": "'+aPill.frequency+'", "times": "'+aPill.times+'", "lasttake": ['+aPill.lasttake+']}';
+		return pillString;
+	}
 
 
-    function writePill(aPill) {
+	function writePill(aPill) {
 
-        // JS GLOBAL WRITE
-        // if edit aPill, must delete aPill first before writing to js global and .JSON
-        var index=-1;
-        for (var i=0; i<myJsonPills.length; i++) {
-            if (aPill.name==myJsonPills[i].name) {
-                index=i;
-            }
-        }
-        if (index!=-1) { // if index==-1, is add not edit
-            myJsonPills[index]=aPill;
-        }
-        else {
-            myJsonPills.push(aPill);
-        }
-
-
-        // .JSON WRITE (slower than read)
-        var oldPillsList=returnAllDrugs(); // returns list of javascript myPill *objects*
-        var newPillsList='['; // string list
-
-        for (var i=0; i<oldPillsList.length; i++) {
-            if (i!=0) {
-                    newPillsList+=', ';
-                }
-
-            if (aPill.name!=oldPillsList[i].name) { // if aPill existed previously, erase old info to write new info == don't write it
-                var stringifyPill=pillToJsonString(oldPillsList[i]);
-                newPillsList+=stringifyPill;
-            }
-            else { // edit
-                var stringifyPill=pillToJsonString(aPill);
-                newPillsList+=stringifyPill;
-            }
-        }
+		// JS GLOBAL WRITE
+		// if edit aPill, must delete aPill first before writing to js global and .JSON
+		var index=-1;
+		for (var i=0; i<myJsonPills.length; i++) {
+			if (aPill.name==myJsonPills[i].name) {
+				index=i;
+			}
+		}
+		if (index!=-1) { // if index==-1, is add not edit
+			myJsonPills[index]=aPill;
+			alert("edit " + i);
+		}
+		else {
+			myJsonPills.push(aPill);
+			alert("add " + i);
+		}
 
 
-        if (index!=-1) { // if index==-1, is add not edit
-            if (oldPillsList.length!=0) {
-                newPillsList+=', ';
-            }
-            var stringifyPill=pillToJsonString(aPill);
-            newPillsList+=stringifyPill;
-        }
+		// .JSON WRITE (slower than read)
+		var oldPillsList=returnAllDrugs(); // returns list of javascript myPill *objects*
+		var newPillsList='['; // string list
 
-        newPillsList+=']';
+		for (var i=0; i<oldPillsList.length; i++) {
+			if (i!=0) {
+					newPillsList+=', ';
+				}
 
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePill', 'input': newPillsList });
-        updateManagePills(myJsonPills);
-    }
-
-
-    function unwritePill(aPill) {
-
-        // JS GLOBAL WRITE
-        // if edit aPill, must delete aPill first before writing to js global and .JSON
-
-        var index=-1;
-        for (var i=0; i<myJsonPills.length; i++) {
-            if (aPill.name==myJsonPills[i].name) {
-                index=i;
-            }
-        }
-        if (index!=-1) { // if index==-1, is add not edit
-            myJsonPills.splice(index, 1); // SPLICE HERE
-        }
-        else {
-            alert(aPill.name + " does not exist? This should not happen..");
-        }
+			if (aPill.name!=oldPillsList[i].name) { // if aPill existed previously, erase old info to write new info == don't write it
+				var stringifyPill=pillToJsonString(oldPillsList[i]);
+				newPillsList+=stringifyPill;
+			}
+			else { // edit
+				var stringifyPill=pillToJsonString(aPill);
+				newPillsList+=stringifyPill;
+			}
+		}
 
 
-        // .JSON WRITE (slower than read)
-        var oldPillsList=returnAllDrugs();
-        var newPillsList='[';
+		if (index!=-1) { // if index==-1, is add not edit
+			if (oldPillsList.length!=0) {
+				newPillsList+=', ';
+			}
+			var stringifyPill=pillToJsonString(aPill);
+			newPillsList+=stringifyPill;
+		}
 
-        for (var i=0; i<oldPillsList.length; i++) {
-            if (aPill.name!=oldPillsList[i].name) {
-                if (i!=0) {
-                    newPillsList+=', ';
-                }
-                newPillsList+='{"name": "'+oldPillsList[i].name+'", "dose": "'+oldPillsList[i].dose+'", "startdate": "'+oldPillsList[i].startdate+'", "enddate": "'+oldPillsList[i].enddate+'", "frequency": "'+oldPillsList[i].frequency+'", "times": "'+oldPillsList[i].times+'", "lasttake": "'+oldPillsList[i].lasttake+'"}';
-            }
-        }
-        newPillsList+=']';
-        
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePill', 'input': newPillsList } );
+		newPillsList+=']';
 
-        updateManagePills(myJsonPills); // interesting. this is not necessary because of deletePill?
-    }
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePill', 'input': newPillsList });
+		updateManagePills(myJsonPills);
+	}
 
 
-    function writeMsg(aMsg) {
-        var oldMsgsList=returnAllMessages();
-        oldMsgsList.push(aMsg);
+	function unwritePill(aPill) {
 
-        var newMsgsList='[';
+		// JS GLOBAL WRITE
+		// if edit aPill, must delete aPill first before writing to js global and .JSON
 
-        for (var i=0; i<oldMsgsList.length; i++) {
-            if (i!=0) {
-                newMsgsList+=', ';
-            }
-            newMsgsList+='{"from": "'+oldMsgsList[i].from+'", "time": "'+oldMsgsList[i].time+'", "message": "'+oldMsgsList[i].message+'", "read": "'+oldMsgsList[i].read+'"}';
-        }
-        newMsgsList+=']';
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMsg', 'input': newMsgsList });
-    }
-
-
-    function changeBoolMsg(aMsg, bool) {
-        var oldMsgsList=returnAllMessages();
-        var updatedMsgsList=[];    
-
-        for (var i=0; i<oldMsgsList.length; i++) {
-            if (aMsg.message==oldMsgsList[i].message) {
-                var newAMsg = new myMessage(oldMsgsList[i].from, oldMsgsList[i].time, oldMsgsList[i].message, bool);
-                updatedMsgsList.push(newAMsg);
-            }
-            else {
-                updatedMsgsList.push(oldMsgsList[i]);
-            }
-        }
-        
-
-        var newMsgsList='[';
-        for (var i=0; i<updatedMsgsList.length; i++) {
-            if (i!=0) {
-                newMsgsList+=', ';
-            }
-            newMsgsList+='{"from": "'+updatedMsgsList[i].from+'", "time": "'+updatedMsgsList[i].time+'", "message": "'+updatedMsgsList[i].message+'", "read": "'+updatedMsgsList[i].read+'"}';
-        }
-        newMsgsList+=']';
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMsg', 'input': newMsgsList });
-        
-    }
+		var index=-1;
+		for (var i=0; i<myJsonPills.length; i++) {
+			if (aPill.name==myJsonPills[i].name) {
+				index=i;
+			}
+		}
+		if (index!=-1) { // if index==-1, is add not edit
+			myJsonPills.splice(index, 1); // SPLICE HERE
+		}
+		else {
+			alert(aPill.name + " does not exist? This should not happen..");
+		}
 
 
-    function writeMissedDrugEvent(missedEvent) {
-        var oldMissedList=returnMissedDrugs();
-        oldMissedList.push(missedEvent);
+		// .JSON WRITE (slower than read)
+		var oldPillsList=returnAllDrugs();
+		var newPillsList='[';
 
-        var newMissedList='[';
+		for (var i=0; i<oldPillsList.length; i++) {
+			if (aPill.name!=oldPillsList[i].name) {
+				if (i!=0) {
+					newPillsList+=', ';
+				}
+				newPillsList+='{"name": "'+oldPillsList[i].name+'", "dose": "'+oldPillsList[i].dose+'", "startdate": "'+oldPillsList[i].startdate+'", "enddate": "'+oldPillsList[i].enddate+'", "frequency": "'+oldPillsList[i].frequency+'", "times": "'+oldPillsList[i].times+'", "lasttake": "'+oldPillsList[i].lasttake+'"}';
+			}
+		}
+		newPillsList+=']';
+		
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePill', 'input': newPillsList } );
 
-        for (var i=0; i<oldMsgsList.length; i++) {
-            if (i!=0) {
-                newMissedList+=', ';
-            }
-            newMissedList+='{"name": "'+oldMissedList[i].name+'", "date": "'+oldMissedList[i].date+'", "dosage": "'+oldMissedList[i].dosage+'", "dateString": "'+oldMissedList[i].dateString+'", "timeString": "'+oldMissedList[i].timeString+'", "state": "missed"}';
-        }
-        newMissedList+=']';
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMissedDrugEvent', 'input': newMissedList });
-
-    }
+		updateManagePills(myJsonPills); // interesting. this is not necessary because of deletePill?
+	}
 
 
-    function changePatientInfo(newInfo) {
-        var newPatientInfo = '[{"email": "'+newInfo.email+'", "name": "'+newInfo.name+'", "age": "'+newInfo.age+'", "info": "'+newInfo.info+'", "connect": "'+newInfo.connect+'"}]';
+	function writeMsg(aMsg) {
+		var oldMsgsList=returnAllMessages();
+		oldMsgsList.push(aMsg);
 
-        $.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePatient', 'input': newPatientInfo });
-    }
+		var newMsgsList='[';
+
+		for (var i=0; i<oldMsgsList.length; i++) {
+			if (i!=0) {
+				newMsgsList+=', ';
+			}
+			newMsgsList+='{"from": "'+oldMsgsList[i].from+'", "time": "'+oldMsgsList[i].time+'", "message": "'+oldMsgsList[i].message+'", "read": "'+oldMsgsList[i].read+'"}';
+		}
+		newMsgsList+=']';
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMsg', 'input': newMsgsList });
+	}
+
+
+	function changeBoolMsg(aMsg, bool) {
+		var oldMsgsList=returnAllMessages();
+		var updatedMsgsList=[];	
+
+		for (var i=0; i<oldMsgsList.length; i++) {
+			if (aMsg.message==oldMsgsList[i].message) {
+				var newAMsg = new myMessage(oldMsgsList[i].from, oldMsgsList[i].time, oldMsgsList[i].message, bool);
+				updatedMsgsList.push(newAMsg);
+			}
+			else {
+				updatedMsgsList.push(oldMsgsList[i]);
+			}
+		}
+		
+
+		var newMsgsList='[';
+		for (var i=0; i<updatedMsgsList.length; i++) {
+			if (i!=0) {
+				newMsgsList+=', ';
+			}
+			newMsgsList+='{"from": "'+updatedMsgsList[i].from+'", "time": "'+updatedMsgsList[i].time+'", "message": "'+updatedMsgsList[i].message+'", "read": "'+updatedMsgsList[i].read+'"}';
+		}
+		newMsgsList+=']';
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMsg', 'input': newMsgsList });
+		
+	}
+
+
+	function writeMissedDrugEvent(missedEvent) {
+		var oldMissedList=returnMissedDrugs();
+		oldMissedList.push(missedEvent);
+
+		var newMissedList='[';
+
+		for (var i=0; i<oldMsgsList.length; i++) {
+			if (i!=0) {
+				newMissedList+=', ';
+			}
+			newMissedList+='{"name": "'+oldMissedList[i].name+'", "date": "'+oldMissedList[i].date+'", "dosage": "'+oldMissedList[i].dosage+'", "dateString": "'+oldMissedList[i].dateString+'", "timeString": "'+oldMissedList[i].timeString+'", "state": "missed"}';
+		}
+		newMissedList+=']';
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writeMissedDrugEvent', 'input': newMissedList });
+
+	}
+
+
+	function changePatientInfo(newInfo) {
+		var newPatientInfo = '[{"email": "'+newInfo.email+'", "name": "'+newInfo.name+'", "age": "'+newInfo.age+'", "info": "'+newInfo.info+'", "connect": "'+newInfo.connect+'"}]';
+
+		$.post('/take-my-pills/src/writeToJson.php', { 'function': 'writePatient', 'input': newPatientInfo });
+	}
